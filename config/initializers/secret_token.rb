05d1625007b0e2df11f69ddee0dd7f5241b2e7c3
Jeff Dickey <dickeytk@Jeff-Dickeys-iMac.local>
2010-11-13 17:05:50 -0800

    added initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twentyone::Application.config.secret_token = '16b3be81d220cad953b98b766e985dc1317bdfb870edd501830a285439c295afab41854c7373e1baaf196f458f8cba3346864ec236df48f65087f3bb474476ba'
