9be084b530825b59fbef7cdfcfb987b0e649bc9f
Jeff Dickey <dickeytk@gmail.com>
2010-11-16 20:48:22 -0800

    fixed twilio

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -2,9 +2,9 @@ class TwilioController < ApplicationController
   :skip_before_filter :require_login :sms
   def sms
     unless request.post?
-      return render { :nothing => true, :status => 404 }
+      return render :nothing => true, :status => 404
     end
     logger.info params.inspect
-    return render { :nothing => true, :status => 200 }
+    return render :nothing => true
   end
 end
