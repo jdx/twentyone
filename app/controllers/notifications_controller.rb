6e5a3ecc26915a8c90396f47c931480a89984ede
Jeff Dickey <dickeytk@gmail.com>
2010-11-16 22:46:50 -0800

    fixed sending cron

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,11 @@
 class NotificationsController < ApplicationController
+  skip_before_filter :require_login, :only => :send
+
   def send
+    unless request.post?
+      return render :nothing => true, :status => 404
+    end
     logger.info 'Sending notifications...'
-    render :text => 'sending notifications', :content_type => 'text/plain'
+    render :text => 'Sent notifications.', :content_type => 'text/plain'
   end
 end
