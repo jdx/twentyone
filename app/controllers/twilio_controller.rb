132edff860f4483eeaac8822fa72da7762a25af9
Jeff Dickey <dickeytk@gmail.com>
2010-11-16 20:45:05 -0800

    removed login requirement for sms

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,8 +1,10 @@
 class TwilioController < ApplicationController
+  :skip_before_filter :require_login :sms
   def sms
     unless request.post?
-      render_404
+      return render { :nothing => true, :status => 404 }
     end
     logger.info params.inspect
+    return render { :nothing => true, :status => 200 }
   end
 end
