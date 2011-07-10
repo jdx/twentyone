158087d4ab6729fbd7ae9a599ec34fef56aae767
Jeff Dickey <dickeytk@gmail.com>
2011-07-10 00:58:57 -0700

    remove post force for notification sending

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,9 +4,6 @@ class NotificationsController < ApplicationController
   skip_before_filter :require_login, :only => :send_all
 
   def send_all
-    unless request.post?
-      return render :nothing => true, :status => 404
-    end
     logger.info 'Sending notifications...'
     Habit.where('next_notification < ?', DateTime.now).each do |habit|
       habit.send_notification
